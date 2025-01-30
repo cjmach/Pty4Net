fba181d9a6aee7e8f6c63e919bb81a8791066f46
Carlos Machado <58347209+cjmach@users.noreply.github.com>
2025-01-30 16:15:35 +0000

    Add unit tests.

---
+++ Pty4Net.Tests/PseudoTerminalTests.cs
@@ -0,0 +1,92 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Pty4Net.Tests;
+
+public class PseudoTerminalTests
+{
+    private const string OutputFileName = "pty-output.txt";
+    private readonly string OutputFile = Path.Combine(Path.GetDirectoryName(typeof(PseudoTerminalTests).Assembly.Location), OutputFileName);
+
+    [SetUp]
+    public void SetUp()
+    {
+        if (File.Exists(OutputFile))
+        {
+            File.Delete(OutputFile);
+        }
+    }
+
+    [Test]
+    public void TestTerminal()
+    {
+        bool exited = false;
+        // output FileStream is used to save the terminal output.
+        using FileStream output = File.OpenWrite(OutputFile);
+        using IPseudoTerminal terminal = PseudoTerminalProvider.CreatePseudoTerminal();
+        terminal.ProcessExited += (sender, e) => exited = true;
+        CancellationTokenSource cancellationSource = new CancellationTokenSource();
+        Task.Run(async () =>
+        {
+            byte[] data = new byte[4096];
+            while (!cancellationSource.IsCancellationRequested)
+            {
+                // read terminal output.
+                var bytesReceived = await terminal.ReadAsync(data, 0, data.Length);
+                if (bytesReceived > 0)
+                {
+                    // save terminal output data to file.
+                    output.Write(data, 0, bytesReceived);
+                }
+                await Task.Delay(5);
+            }
+        }, cancellationSource.Token);
+
+        // generate a random message to be echoed to the terminal.
+        int random = RandomNumberGenerator.GetInt32(int.MaxValue);
+        string outputToMatch = $"Testing {random}";
+
+        // send 'echo' command.
+        byte[] enter = new byte[] { (byte)'\n' };
+        byte[] echoCmd = Encoding.Default.GetBytes($"echo {outputToMatch}");
+        terminal.WriteAsync(echoCmd, 0, echoCmd.Length).Wait();
+        terminal.WriteAsync(enter, 0, enter.Length).Wait();
+
+        // send 'exit' command.
+        byte[] exitCmd = Encoding.Default.GetBytes("exit");
+        terminal.WriteAsync(exitCmd, 0, exitCmd.Length).Wait();
+        terminal.WriteAsync(enter, 0, enter.Length).Wait();
+
+        // wait for terminal to exit.
+        Task.Run(async () =>
+        {
+            while (!exited)
+            {
+                await Task.Delay(5);
+            }
+            output.Flush();
+            output.Close();
+            cancellationSource.Cancel();
+        }).Wait(5000);
+
+        bool ok = false;
+        string line;
+        // read output, line by line.
+        using StreamReader reader = File.OpenText(OutputFile);
+        while ((line = reader.ReadLine()) != null)
+        {
+            if (outputToMatch.Equals(line))
+            {
+                ok = true;
+                break;
+            }
+        }
+
+        Assert.IsTrue(ok, $"Line '{outputToMatch}' not found in the terminal output.");
+    }
+}
