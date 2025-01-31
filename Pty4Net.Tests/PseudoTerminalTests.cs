cdc117071e74288946171fff2f7c37c6cec89938
Carlos Machado <58347209+cjmach@users.noreply.github.com>
2025-01-31 00:10:03 +0000

    Fix unit test.

--- Pty4Net.Tests/PseudoTerminalTests.cs
+++ Pty4Net.Tests/PseudoTerminalTests.cs
@@ -44,73 +44,76 @@ public class PseudoTerminalTests
     public void TestTerminal()
     {
         bool exited = false;
-
-        PseudoTerminalOptions options = PseudoTerminalOptions.CreateDefault();
-        options.Columns = 80;
-        options.Rows = 40;
-        
-        // output FileStream is used to save the terminal output.
-        using FileStream output = File.OpenWrite(OutputFile);
-        using IPseudoTerminal terminal = PseudoTerminalProvider.CreatePseudoTerminal(options);
-        terminal.ProcessExited += (sender, e) => exited = true;
-        CancellationTokenSource cancellationSource = new CancellationTokenSource();
-        Task.Run(async () =>
-        {
-            byte[] data = new byte[4096];
-            while (!cancellationSource.IsCancellationRequested)
-            {
-                // read terminal output.
-                var bytesReceived = await terminal.ReadAsync(data, 0, data.Length);
-                if (bytesReceived > 0)
-                {
-                    // save terminal output data to file.
-                    output.Write(data, 0, bytesReceived);
-                }
-                await Task.Delay(5);
-            }
-        }, cancellationSource.Token);
-
-        Thread.Sleep(500);
+        bool ok = false;
 
         // generate a random message to be echoed to the terminal.
         int random = RandomNumberGenerator.GetInt32(int.MaxValue);
         string outputToMatch = $"Testing {random}";
 
-        // send 'echo' command.
-        byte[] enter = Encoding.Default.GetBytes(Environment.NewLine);
-        byte[] echoCmd = Encoding.Default.GetBytes($"echo {outputToMatch}");
-        terminal.WriteAsync(echoCmd, 0, echoCmd.Length).Wait();
-        terminal.WriteAsync(enter, 0, enter.Length).Wait();
+        PseudoTerminalOptions options = PseudoTerminalOptions.CreateDefault();
+        options.Columns = 80;
+        options.Rows = 40;
 
-        Thread.Sleep(500);
-
-        // send 'exit' command.
-        byte[] exitCmd = Encoding.Default.GetBytes("exit");
-        terminal.WriteAsync(exitCmd, 0, exitCmd.Length).Wait();
-        terminal.WriteAsync(enter, 0, enter.Length).Wait();
-
-        // wait for terminal to exit.
-        Task.Run(async () =>
+        // output FileStream is used to save the terminal output.
+        using (FileStream output = File.OpenWrite(OutputFile))
         {
-            while (!exited)
+            using IPseudoTerminal terminal = PseudoTerminalProvider.CreatePseudoTerminal(options);
+            terminal.ProcessExited += (sender, e) => exited = true;
+            CancellationTokenSource cancellationSource = new CancellationTokenSource();
+            Task.Run(async () =>
             {
-                await Task.Delay(5);
-            }
-            output.Flush();
-            output.Close();
-            cancellationSource.Cancel();
-        }).Wait(5000);
+                byte[] data = new byte[4096];
+                while (!cancellationSource.IsCancellationRequested)
+                {
+                    // read terminal output.
+                    var bytesReceived = await terminal.ReadAsync(data, 0, data.Length);
+                    if (bytesReceived > 0)
+                    {
+                        // save terminal output data to file.
+                        output.Write(data, 0, bytesReceived);
+                    }
+                    await Task.Delay(5);
+                }
+            }, cancellationSource.Token);
 
-        bool ok = false;
-        string line;
-        // read output, line by line.
-        using StreamReader reader = File.OpenText(OutputFile);
-        while ((line = reader.ReadLine()) != null)
-        {
-            if (outputToMatch.Equals(line))
+            Thread.Sleep(500);
+
+            // send 'echo' command.
+            byte[] enter = Encoding.Default.GetBytes(Environment.NewLine);
+            byte[] echoCmd = Encoding.Default.GetBytes($"echo {outputToMatch}");
+            terminal.WriteAsync(echoCmd, 0, echoCmd.Length).Wait();
+            terminal.WriteAsync(enter, 0, enter.Length).Wait();
+
+            Thread.Sleep(500);
+
+            // send 'exit' command.
+            byte[] exitCmd = Encoding.Default.GetBytes("exit");
+            terminal.WriteAsync(exitCmd, 0, exitCmd.Length).Wait();
+            terminal.WriteAsync(enter, 0, enter.Length).Wait();
+
+            // wait for terminal to exit.
+            Task.Run(async () =>
             {
-                ok = true;
-                break;
+                while (!exited)
+                {
+                    await Task.Delay(5);
+                }
+                output.Flush();
+                output.Close();
+                cancellationSource.Cancel();
+            }).Wait(5000);
+
+            
+            string line;
+            // read output, line by line.
+            using StreamReader reader = File.OpenText(OutputFile);
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (outputToMatch.StartsWith(line))
+                {
+                    ok = true;
+                    break;
+                }
             }
         }
 
